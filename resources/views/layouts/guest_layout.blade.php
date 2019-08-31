71e6d77f3dba1036582db0aeef2c382e55f02213
Trufan Andreea <andreea.trufan@gmail.com>
2019-08-31 15:47:09 +0300

    initial commit

---
+++ resources/views/layouts/guest_layout.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tiny Blog</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <style>
+            header{
+                width: 100%;
+                height: 56.24vw;
+            }
+            header>img{
+                width: 100%;
+                height: 56.24vw;
+                /* object-fit: cover; */
+            }
+            body{
+                background: linear-gradient(to left, #f2f2f2, #ececec);
+            }
+            .overlay{
+                width: 100%;
+                height: 100%;
+                position: relative;
+                padding: 0 20% 0 20%;
+                top: -100%;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+            }
+            .effect-container{
+                background: #000;
+                width: 320px;
+                height: 120px;
+                color: #fff;
+                display: flex;
+                align-items: center;
+                padding-left: 30px;
+                font-size: 3rem;
+            }
+            .effect-container>p{
+                margin: 0;
+                padding-right: 25px;
+                animation-name: text-bar;
+                animation-duration: 1.2s;
+                animation-iteration-count: infinite;
+                border-right: none;
+            }
+
+            @keyframes text-bar{
+                100%{
+                    border-right: 3px solid #fff;
+                }
+            }
+            .subtitle{
+                font-size: 1.5rem;
+                margin-top: 30px;
+            }
+        </style>
+
+    </head>
+    <body>
+        <header>
+            <img src="{{ asset('/images/tiny-blog.jpg') }}" alt="Tinyblog cover photo" />
+            <div class="overlay">
+                <div>
+                    <div class="effect-container">
+                        <p>A tiny blog</p>
+                    </div>
+                    <p class="subtitle">Simplicity is the ultimate sophistication.</p>
+                </div>
+            </div>
+        </header>
+        <main class='container'>
+            @yield('content')
+        </main>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.guest_layout')
+
+@section('content')
+    <h1>Welcome page</h1>
+@endsection
