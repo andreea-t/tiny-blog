bb871766559399d48a9909b1b411fbe2474f62ba
andreea-t <andreea.trufan@gmail.com>
2020-01-22 17:26:25 +0200

    Added email verification

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -13,10 +13,11 @@ class PostsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('verified');
+    }
 
     public function index()
     {
